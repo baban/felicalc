9962b9d22f8b1943769dea68831723a323a46bb1
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-02-17 13:49:54 +0900

    git project作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JpFullstack::Application.config.secret_token = '8f886bf100fd32624ea3917b1beb7f79e8ff165f561765d430465c34516da967be17f563d479b557620a6932861fd2fc1d9240eca42b1219ec348ff547882b1d'
