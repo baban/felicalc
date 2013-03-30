278daf28257a7ccd8a81f7090648e78f5faad239
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-03-30 12:11:12 +0900

    掲示板調整

--- app/controllers/account_books_controller.rb
+++ app/controllers/account_books_controller.rb
@@ -34,27 +34,6 @@ class AccountBooksController < ApplicationController
     end
   end
   
-  # 更新
-  def update
-    @account_book = AccountBook.find(params[:id])
-    respond_to do |format|
-      if @account_book.update_attributes(params[:account_book])
-        flash[:notice] = 'データ更新完了!'
-        format.html { redirect_to(@account_book) }
-      else
-        format.html { render :action => "edit" }
-      end
-    end
-  end
-  
-  # 破棄
-  def destroy
-    @account_book = AccountBook.find(params[:id])
-    @account_book.destroy
-    
-    respond_to { |fmt| fmt.html { redirect_to(account_books_url) } }
-  end
-  
   # 指定された条件の列をすべて取り出して、jsonで返す
   def get_row
     @account_books = AccountBook.where( user_id: current_user.id ).where( params[:conditions] ).to_a
@@ -82,16 +61,10 @@ class AccountBooksController < ApplicationController
   def export
     # 日付の解析
     now = DateTime.now
-    all, start_date, end_date = params[:id].match(/(\d+)-(\d+)/).to_a
+    ori, start_date, end_date = params[:id].match(/(\d+)-(\d+)/).to_a
     start_date, end_date = DateTime.parse(start_date), DateTime.parse(end_date)
     
-    h = {
-      :uid => @u.id,
-      :start => start_date.beginning_of_day,
-      :end => end_date.end_of_day,
-    } 
-    @account_books =  AccountBook.all( 
-      :conditions=> [ ' user_id = :uid and date between :start and :end ', h ] )
+    @account_books = AccountBook.where( user_id: current_user.id ).where( date: start_date.beginning_of_day..end_date.end_of_day )
     
     respond_to { |fmt|
       cls = [:date,:money,:usecase,:memo,:cardtype,:category]
